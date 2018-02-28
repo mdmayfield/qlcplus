e6894b17f0178767a38e4bc2ae5ddc7c2cc861a7
mdmayfield <mattmayfield@Matts-iMac.domain>
2018-02-27 22:35:37 -0600

    Bump version number

---
+++ engine/src/qlcconfig.h
@@ -0,0 +1,23 @@
+# ifndef CONFIG_H
+# define CONFIG_H
+# define APPNAME "Q Light Controller Plus"
+# define FXEDNAME "Fixture Definition Editor"
+# define APPVERSION "MM Custom-201803WIP"
+# define DOCSDIR "Resources/Documents"
+# define INPUTPROFILEDIR "Resources/InputProfiles"
+# define USERQLCPLUSDIR "Library/Application Support/QLC+"
+# define USERINPUTPROFILEDIR "Library/Application Support/QLC+/InputProfiles"
+# define MIDITEMPLATEDIR "Resources/MidiTemplates"
+# define USERMIDITEMPLATEDIR "Library/Application Support/QLC+/MidiTemplates"
+# define MODIFIERSTEMPLATEDIR "Resources/ModifiersTemplates"
+# define USERMODIFIERSTEMPLATEDIR "Library/Application Support/QLC+/ModifiersTemplates"
+# define FIXTUREDIR "Resources/Fixtures"
+# define USERFIXTUREDIR "Library/Application Support/QLC+/Fixtures"
+# define PLUGINDIR "PlugIns"
+# define AUDIOPLUGINDIR "PlugIns/Audio"
+# define TRANSLATIONDIR "Resources/Translations"
+# define RGBSCRIPTDIR "Resources/RGBScripts"
+# define USERRGBSCRIPTDIR "Library/Application Support/QLC+/RGBScripts"
+# define GOBODIR "Resources/Gobos"
+# define WEBFILESDIR "Resources/Web"
+# endif
